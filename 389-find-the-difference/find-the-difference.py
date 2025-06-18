7bc241c7fe0e0294ec27ce7f103f3fa8cb3a4868
Archu1416 <archanadanam@gmail.com>
2025-06-18 23:24:30 +0530

    Time: 2 ms (61.06%) | Memory: 17.9 MB (43.95%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s,t=sorted(s),sorted(t)
+        for i,j in zip(s,t):
+            if i!=j:
+                return j
+        return t[-1]
