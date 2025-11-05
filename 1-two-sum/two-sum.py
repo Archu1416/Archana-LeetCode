280b09478dc0efcb066bcca91a593b39f78ce496
Archu1416 <archanadanam@gmail.com>
2025-11-05 22:44:04 +0530

    Time: 8 ms (47.76%) | Memory: 13.5 MB (15.92%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,18 @@
-class Solution:
-    def twoSum(self, arr: List[int], target: int) -> List[int]:
-        hash={}
-        for i,v in enumerate(arr):
-            r=target-v
-            if r in hash:
-                return [hash[r],i]
-            hash[v]=i
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums=[(num,i) for i,num in enumerate(nums)]
+        nums.sort()
+        l=0
+        r=len(nums)-1
+        while l!=r:
+            if nums[l][0]+nums[r][0]<target:
+                l+=1
+            elif nums[l][0]+nums[r][0]>target:
+                r-=1
+            else:
+                return [nums[l][1],nums[r][1]]
